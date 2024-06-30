451b3b4b2a56a5ecf932e2ad3f4dbe841107c0dd
David Henrique <dh210605@gmail.com>
2024-06-29 23:53:27 -0300

    teste de integração

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import './App.css'
-import Form from './components/Form'
-import Header from './components/Header'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
+  const [data, setData] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<string>('http://localhost:8080/api/hello');
+        setData(response.data);
+      } catch (error) {
+        setError('Erro ao buscar a mensagem da API');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
-    <>
     <div className="App">
-      <h1 style={{textAlign: 'center'}}>
-        <Header/>
-      </h1>
-      <main style={{textAlign: 'center'}}>
-        <Form/>
-      </main>
-      <footer>
-      </footer>
+      <h1>{data || 'Sem dados'}</h1>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
