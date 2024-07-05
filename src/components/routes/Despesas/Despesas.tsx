b781402388479741bfc63596ceb3392821d28dd1
David Henrique <dh210605@gmail.com>
2024-07-04 23:31:24 -0300

    aba de despesas, e toast

--- src/components/routes/Despesas/Despesas.tsx
+++ src/components/routes/Despesas/Despesas.tsx
@@ -1,9 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import styles from './Despesas.module.css';
+import styles from "./Despesas.module.css";
+import { useState } from "react";
+import { Card, Space } from "antd";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Despesas = () => {
+  const [despesas, setDespesas] = useState<number | undefined>(undefined);
+
+  const handledespesas = () => {
+    if (despesas !== undefined) {
+      console.log(despesas);
+      toast.success("Despesa adicionada");
+      setDespesas(undefined);
+    } else {
+      toast.error("Adicione uma despesa");
+    }
+  };
+
   return (
     <div>
       <h1>
@@ -11,6 +27,45 @@ const Despesas = () => {
           <FontAwesomeIcon icon={faArrowLeft} />
         </Link>
       </h1>
+
+      <div className="Card">
+        <Space direction="vertical" size={16}>
+          <Card
+            title="Sua despesa"
+            style={{ width: 300, border: "1px solid black" }}
+            headStyle={{ borderBottom: "1px solid black" }}
+          >
+            <input
+              type="number"
+              value={despesas ?? ""}
+              onChange={(e) => setDespesas(Number(e.target.value))}
+              placeholder="$$$"
+              style={{
+                position: "relative",
+                right: "-35px",
+                border: "1px solid black",
+                borderRadius: "4px",
+              }}
+            />
+            <br />
+            <br />
+            <br />
+            <button
+              onClick={handledespesas}
+              style={{
+                borderRadius: "10px",
+                position: "relative",
+                right: "-85px",
+                top: "20px",
+              }}
+            >
+              Adicionar
+            </button>
+          </Card>
+        </Space>
+      </div>
+
+      <ToastContainer />
     </div>
   );
 };
--- src/components/routes/Faturamento/Faturamento.tsx
+++ src/components/routes/Faturamento/Faturamento.tsx
@@ -9,11 +9,10 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Faturamento: React.FC = () => {
   const [faturamento, setFaturamento] = useState<number | undefined>(undefined);
-;
   const handleAdicionar = () => {
     if (faturamento !== undefined) {
       console.log(faturamento);
-      toast.success("Sucesso!");
+      toast.success("Faturamento adicionado!!");
       setFaturamento(undefined);
     } else {
       toast.error("Digite um valor!");
@@ -32,22 +31,32 @@ const Faturamento: React.FC = () => {
         <Space direction="vertical" size={16}>
           <Card
             title="Seu faturamento"
-            style={{ width: 300, border: '1px solid black' }}
-            headStyle={{ borderBottom: '1px solid black' }}
+            style={{ width: 300, border: "1px solid black" }}
+            headStyle={{ borderBottom: "1px solid black" }}
           >
             <input
               type="number"
-              value={faturamento ?? ''}
+              value={faturamento ?? ""}
               onChange={(e) => setFaturamento(Number(e.target.value))}
               placeholder="$$$"
-              style={{ position: 'relative', right: '-35px', border: '1px solid black', borderRadius: '4px' }}
-            /> 
+              style={{
+                position: "relative",
+                right: "-35px",
+                border: "1px solid black",
+                borderRadius: "4px",
+              }}
+            />
             <br />
             <br />
             <br />
             <button
               onClick={handleAdicionar}
-              style={{ borderRadius: '10px', position: 'relative', right: '-85px', top: '20px' }}
+              style={{
+                borderRadius: "10px",
+                position: "relative",
+                right: "-85px",
+                top: "20px",
+              }}
             >
               Adicionar
             </button>
